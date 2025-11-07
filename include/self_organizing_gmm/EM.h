6f9eca260d6fdbfc219f7ef2aa9719a0ba35f2d6
Tomasz Niedzialkowski <tomek_niedzi@o2.pl>
2025-11-07 15:52:49 +0100

    exposed learner params: tol, reg_covar and max_iter through pybind

--- include/self_organizing_gmm/EM.h
+++ include/self_organizing_gmm/EM.h
@@ -39,11 +39,11 @@ namespace sogmm
       using MatrixDX = typename Container::MatrixDX;
       using MatrixXC = typename Container::MatrixXC;
 
-      EM() : EM(1e-3, 1e-6, 100)
-      {
-      }
+      // EM() : EM(1e-3, 1e-6, 100)
+      // {
+      // }
 
-      EM(T tol, T reg_covar, unsigned int max_iter)
+      EM(T tol = 1e-3, T reg_covar = 1e-6, unsigned int max_iter = 100)
       {
         tol_ = tol;
         reg_covar_ = reg_covar;
